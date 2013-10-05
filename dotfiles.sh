4ce32cdfaf9e6deeb037e67da7ded44790f5d8d2
Robin Kautz <robin.kautz@devbliss.com>
2013-10-05 14:46:25 +0200

    ignore .vim dirs exist

--- dotfiles.sh
+++ dotfiles.sh
@@ -18,8 +18,8 @@ for i in $(find $repo/conf -depth 1); do
     ln -sf $repo/conf/$file ~/$file
 done
 
-mkdir ~/.vim
-mkdir ~/.vimvar
+mkdir -p ~/.vim
+mkdir -p ~/.vimvar
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 exit 0
