380de7b0cb7b9986a9c1ba45a22c25ebc9027f1c
Robin Kautz <bob3000@users.noreply.github.com>
2021-08-04 09:43:54 +0200

    reordered file contents

--- conf/lv-config.lua
+++ conf/lv-config.lua
@@ -7,49 +7,61 @@ vim.o.guifont = "Fira Code:h10"
 vim.o.colorcolumn = "80,120"
 vim.o.relativenumber = true
 
--- keymappings
 lvim.leader = "space"
--- overwrite the key-mappings provided by LunarVim for any mode, or leave it empty to keep them
--- lvim.keys.normal_mode = {
---   Page down/up
---   {'[d', '<PageUp>'},
---   {']d', '<PageDown>'},
---
---   Navigate buffers
---   {'<Tab>', ':bnext<CR>'},
---   {'<S-Tab>', ':bprevious<CR>'},
--- }
--- if you just want to augment the existing ones then use the utility function
--- require("lv-utils").add_keymap_insert_mode({ silent = true }, {
--- { "<C-s>", ":w<cr>" },
--- { "<C-c>", "<ESC>" },
--- })
--- you can also use the native vim way directly
--- vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
-
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.dap.active = true
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
 
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {}
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+lvim.autocommands.custom_groups = {
+	{
+		"InsertEnter",
+		"*",
+		"if &relativenumber | let g:ms_relativenumberoff = 1 | setlocal number norelativenumber | endif",
+	},
+	{ "InsertLeave", "*", 'if exists("g:ms_relativenumberoff") | setlocal relativenumber | endif' },
+	{ "InsertEnter", "*", "if &cursorline | let g:ms_cursorlineoff = 1 | setlocal nocursorline | endif" },
+	{ "InsertLeave", "*", 'if exists("g:ms_cursorlineoff") | setlocal cursorline | endif' },
+}
 
--- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
+-- Additional Leader bindings for WhichKey
+lvim.builtin.which_key.mappings["t"] = {
+	name = "Extra Windows",
+	d = { "<cmd>TroubleToggle<CR>", "Togggle Trouble" },
+	o = { "<cmd>SymbolsOutline<CR>", "Togggle Symbols Outline" },
+	m = { "<cmd>MarkdownPreviewToggle<CR>", "Markdown Browser" },
+	t = { "<cmd>TodoTrouble<CR>", "Togggle Todos" },
+	D = { "<cmd>lua require('dapui').toggle()<cr>", "Toggle DAP UI" },
+}
+
+lvim.builtin.which_key.mappings["S"] = {
+	name = "Session",
+	o = { "<cmd>lua require('persistence').load()<cr>", "Open Session" },
+	l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Last Session" },
+	d = { "<cmd>lua require('persistence').stop()<cr>", "Disable Saving" },
+}
+
+lvim.builtin.which_key.mappings["R"] = {
+	name = "Rust Tools",
+	b = { "<cmd>lua require('core.terminal')._exec_toggle('cargo build;read')<CR>", "Cargo build" },
+	r = { "<cmd>lua require('core.terminal')._exec_toggle('cargo run;read')<CR>", "Cargo run" },
+	t = { "<cmd>lua require('core.terminal')._exec_toggle('cargo test;read')<CR>", "Cargo test" },
+	c = { "<cmd>lua require('core.terminal')._exec_toggle('cargo check;read')<CR>", "Cargo check" },
+	m = { "<cmd>RustExpandMacro<CR>", "Expand Macro" },
+	H = { "<cmd>RustToggleInlayHints<CR>", "Inlay Hints" },
+	R = { "<cmd>RustRunnables<CR>", "Runnables" },
+	h = { "<cmd>RustHoverActions<CR>", "Hover Actions" },
+	d = { "<cmd>RustDebuggables<CR>", "Debuggables" },
+}
+
+lvim.builtin.which_key.mappings["r"] = {
+	name = "Replace",
+	r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
+	w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
+	f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
+}
 
 -- Additional Plugins
 lvim.lsp.override = { "rust" }
@@ -146,6 +158,18 @@ lvim.plugins = {
       require('telescope').load_extension('dap')
 		end,
 	},
+  -- session
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    module = "persistence",
+    config = function()
+      require("persistence").setup( {
+        dir = vim.fn.expand(vim.fn.stdpath("cache") .. "/sessions/"), -- directory where session files are saved
+        options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
+      })
+    end,
+  }
   -- language server
 	-- {
 	-- 	"ray-x/lsp_signature.nvim",
@@ -154,48 +178,6 @@ lvim.plugins = {
 	-- 	end,
 	-- 	event = "InsertEnter",
 	-- },
-}
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
-lvim.autocommands.custom_groups = {
-	{
-		"InsertEnter",
-		"*",
-		"if &relativenumber | let g:ms_relativenumberoff = 1 | setlocal number norelativenumber | endif",
-	},
-	{ "InsertLeave", "*", 'if exists("g:ms_relativenumberoff") | setlocal relativenumber | endif' },
-	{ "InsertEnter", "*", "if &cursorline | let g:ms_cursorlineoff = 1 | setlocal nocursorline | endif" },
-	{ "InsertLeave", "*", 'if exists("g:ms_cursorlineoff") | setlocal cursorline | endif' },
-}
-
--- Additional Leader bindings for WhichKey
-lvim.builtin.which_key.mappings["t"] = {
-	name = "Extra Windows",
-	d = { "<cmd>TroubleToggle<CR>", "Togggle Trouble" },
-	o = { "<cmd>SymbolsOutline<CR>", "Togggle Symbols Outline" },
-	m = { "<cmd>MarkdownPreviewToggle<CR>", "Markdown Browser" },
-	t = { "<cmd>TodoTrouble<CR>", "Togggle Todos" },
-	D = { "<cmd>lua require('dapui').toggle()<cr>", "Toggle DAP UI" },
-}
-
-lvim.builtin.which_key.mappings["R"] = {
-	name = "Rust Tools",
-	b = { "<cmd>lua require('core.terminal')._exec_toggle('cargo build;read')<CR>", "Cargo build" },
-	r = { "<cmd>lua require('core.terminal')._exec_toggle('cargo run;read')<CR>", "Cargo run" },
-	t = { "<cmd>lua require('core.terminal')._exec_toggle('cargo test;read')<CR>", "Cargo test" },
-	c = { "<cmd>lua require('core.terminal')._exec_toggle('cargo check;read')<CR>", "Cargo check" },
-	m = { "<cmd>RustExpandMacro<CR>", "Expand Macro" },
-	H = { "<cmd>RustToggleInlayHints<CR>", "Inlay Hints" },
-	R = { "<cmd>RustRunnables<CR>", "Runnables" },
-	h = { "<cmd>RustHoverActions<CR>", "Hover Actions" },
-	d = { "<cmd>RustDebuggables<CR>", "Debuggables" },
-}
-
-lvim.builtin.which_key.mappings["r"] = {
-	name = "Replace",
-	r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
-	w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
-	f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
 }
 
 -- DAP
@@ -220,7 +202,7 @@ lvim.builtin.dap.on_config_done = function(dap)
       name = "Debug",
       request = "launch",
       program = function()
-        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
+        return vim.fn.input('Path to executable: ', CACHE_PATH .. '/target/debug/', 'file')
       end,
       cwd = '${workspaceFolder}',
       stopOnEntry = false,
