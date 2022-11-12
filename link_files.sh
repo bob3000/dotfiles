113638b675ec9948d65a743e5fd58be7fff7f3e7
Robin Kautz <git@bob3000.net>
2022-11-12 21:58:05 +0100

    backup and link function

--- link_files.sh
+++ link_files.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 set -eux
 
+function backup_and_link() {
+  src=$1
+  dest=$2
+  if [ -L "$dest" ]; then
+    return;
+  fi
+
+  if [ -f "$dest" ]; then
+    mv "$dest" "${dest}.bak"
+  fi
+  mkdir -p "$(dirname $dest)"
+  ln -s "$src" "$dest"
+}
+
 function link_common_files() {
-  mkdir -p ${HOME}/.config/kitty
-  mkdir -p ${HOME}/.config/lvim
-  mkdir -p ${HOME}/.zshrc.local.d
-  touch ${HOME}/.zshrc.local.d/.keep
-  ln -s "$PWD/conf/.gitconfig" ${HOME}/.gitconfig
-  ln -s "$PWD/conf/.pythonstartup" ${HOME}/.pythonstartup
-  ln -s "$PWD/conf/.tmux.conf" ${HOME}/.tmux.conf
-  ln -s "$PWD/conf/kitty.conf" ${HOME}/.config/kitty/kitty.conf
-  ln -s "$PWD/conf/starship.toml" ${HOME}/.config/starship.toml
-  ln -s "$PWD/conf/lv-config.lua" ${HOME}/.config/lvim/config.lua
-  ln -s "$PWD/conf/.zshrc" ${HOME}/.zshrc
+  backup_and_link "$PWD/conf/.gitconfig" "${HOME}/.gitconfig"
+  backup_and_link "$PWD/conf/.pythonstartup" "${HOME}/.pythonstartup"
+  backup_and_link "$PWD/conf/.tmux.conf" "${HOME}/.tmux.conf"
+  backup_and_link "$PWD/conf/kitty.conf" "${HOME}/.config/kitty/kitty.conf"
+  backup_and_link "$PWD/conf/starship.toml" "${HOME}/.config/starship.toml"
+  backup_and_link "$PWD/conf/lv-config.lua" "${HOME}/.config/lvim/config.lua"
+  backup_and_link "$PWD/conf/.zshrc" "${HOME}/.zshrc"
 }
 
 function link_mac_files() {
-  ln -s "$PWD/conf/.zshrc.mac" "${HOME}/.zshrc.mac"
+  backup_and_link "$PWD/conf/.zshrc.mac" "${HOME}/.zshrc.mac"
 }
 
 function link_linux_files() {
-  ln -s "$PWD/conf/.zshrc.linux" "${HOME}/.zshrc.linux"
+  backup_and_link "$PWD/conf/.zshrc.linux" "${HOME}/.zshrc.linux"
 }
 
 function main() {
   link_common_files
   if [[ "$OSTYPE" =~ ^darwin ]]; then
-    # TODO: brew install
     link_mac_files
   fi
 
