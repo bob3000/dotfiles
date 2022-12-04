2c0561ad3dc8ec4a6c2e8b369e375f5c1434770c
Robin Kautz <git@bob3000.net>
2022-12-04 15:28:38 +0100

    fish theme and man colors

--- conf/config.fish
+++ conf/config.fish
@@ -1,9 +1,10 @@
-set fish_greeting 
+set fish_greeting
 
 if status is-interactive
   # Commands to run in interactive sessions can go here
   starship init fish | source
   fzf_key_bindings
+  theme_gruvbox dark medium
 end
 
 if set -q $SSH_CONNECTION
@@ -21,7 +22,7 @@ set PATH "$GOPATH/bin:$PATH"
 set PATH "$HOME/.cargo/bin:$PATH"
 set PATH "$HOME/.local/bin:$PATH"
 
-set --universal nvm_default_version lts/hydrogen
+set nvm_default_version lts/hydrogen
 
 # make vim fzf plugin use ripgrep
 set FZF_DEFAULT_COMMAND 'rga --files'
@@ -30,6 +31,12 @@ set FZF_CTRL_T_OPTS '--preview "[[ $(file --mime {}) =~ binary ]] && echo {} is 
 set FZF_ALT_C_COMMAND 'fd -t d . $HOME'
 set FZF_ALT_C_OPTS '--preview "exa -la {}"'
 
+# man colors
+set -g man_blink -o red
+set -g man_bold -o green
+set -g man_standout -b black 93a1a1
+set -g man_underline -u 93a1a1
+
 # convenience
 abbr ls 'ls --color'
 abbr ll 'exa -l'
@@ -37,7 +44,6 @@ abbr la 'exa -a'
 abbr tmux-cwd 'tmux command-prompt -I $PWD -p "New session dir:" "attach -c %1"'
 abbr sudo 'sudo -v; sudo '
 abbr wanip 'curl -s ifconfig.me'
-abbr neovide 'neovide --multigrid -- -u ~/.local/share/lunarvim/lvim/init.lua --cmd "set runtimepath+=~/.local/share/lunarvim/lvim"'
 abbr icat "kitty +kitten icat"
 abbr d "kitty +kitten diff"
 abbr emoji "kitty +kitten unicode_input"
