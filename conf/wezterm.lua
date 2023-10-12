3f3b4f740a880bea23b7ea510d768c6fd96f619f
Robin Kautz <git@bob3000.net>
2023-10-12 16:54:36 +0200

    fix fonts and colors scheme

--- conf/wezterm.lua
+++ conf/wezterm.lua
@@ -1,12 +1,12 @@
 local wezterm = require 'wezterm'
-
 local config = {}
 
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Everforest Dark'
+config.default_prog = { '/usr/bin/fish', '-l' }
+config.color_scheme = 'Everforest Dark (Gogh)'
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font_with_fallback {
   {
@@ -14,9 +14,14 @@ config.font = wezterm.font_with_fallback {
     weight = 'Medium',
     harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' },
   },
-  { family = 'Terminus', weight = 'Bold' },
+  { family = 'Fira Code Nerd Font Mono', weight = 'Medium' },
+  { family = 'DejaVu Sans Mono', weight = 'Medium' },
+  'Noto Serif Tibetan',
+  'Noto Sans Javanese',
+  'Droid Sans Fallback',
+  'Noto Sans Ol Chiki',
   'Noto Color Emoji',
 }
-config.font_size = 15.0
+config.font_size = 13.0
 
 return config
