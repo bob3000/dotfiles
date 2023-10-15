5bcb57dd3dec93298ce3d61d64a45f6a085010f3
Robin Kautz <git@bob3000.net>
2023-10-15 11:52:16 +0200

    keys and colors

--- conf/wezterm.lua
+++ conf/wezterm.lua
@@ -7,6 +7,13 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- domains
+config.unix_domains = {
+  {
+    name = 'unix',
+  },
+}
+
 -- options
 config.scrollback_lines = 10000
 if wezterm.target_triple == "aarch64-apple-darwin" or wezterm.target_triple == "x86_64-apple-darwin" then
@@ -14,7 +21,7 @@ if wezterm.target_triple == "aarch64-apple-darwin" or wezterm.target_triple == "
 elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
 	config.default_prog = { "/usr/bin/fish", "-l" }
 end
-config.hide_tab_bar_if_only_one_tab = true
+config.default_gui_startup_args = { 'connect', 'unix' }
 
 -- colors
 config.color_scheme = "Gruvbox Dark (Gogh)"
@@ -60,34 +67,41 @@ config.window_padding = {
 config.leader = { key="b", mods="CTRL", timeout_milliseconds = 1000 }
 config.keys = {
   -- tmux like key map
-  { key = "b", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
-  { key = "%", mods = "LEADER|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-  { key = "\"",mods = "LEADER|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-  { key = "s", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-  { key = "v", mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+  { key = ':', mods = 'LEADER|SHIFT', action=act.ActivateCommandPalette},
+  { key = "b", mods = "LEADER|CTRL",  action=act{SendString="\x01"}},
+  { key = "[", mods = "LEADER",       action=act.ActivateCopyMode},
+  { key = "]", mods = "LEADER",       action=act.PasteFrom "Clipboard"},
+  { key = "{", mods = "LEADER|SHIFT", action=act.RotatePanes "Clockwise"},
+  { key = "}", mods = "LEADER|SHIFT", action=act.RotatePanes "CounterClockwise"},
+  { key = "t", mods = "LEADER|CTRL",  action=act.QuickSelect},
+  { key = "%", mods = "LEADER|SHIFT", action=act{SplitHorizontal={domain="CurrentPaneDomain"}}},
+  { key = "\"",mods = "LEADER|SHIFT", action=act{SplitVertical={domain="CurrentPaneDomain"}}},
+  { key = "s", mods = "LEADER",       action=act{SplitVertical={domain="CurrentPaneDomain"}}},
+  { key = "v", mods = "LEADER",       action=act{SplitHorizontal={domain="CurrentPaneDomain"}}},
   { key = "o", mods = "LEADER",       action="TogglePaneZoomState" },
   { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
-  { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-  { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
-  { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
-  { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
-  { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
-  { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
-  { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
-  { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
-  { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
-  { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
-  { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
-  { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
-  { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
-  { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
-  { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
-  { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
-  { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
-  { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
-  { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-  { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
-  { key = 's', mods = 'LEADER',       action = act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' }, },
+  { key = "c", mods = "LEADER",       action=act{SpawnTab="CurrentPaneDomain"}},
+  { key = "h", mods = "LEADER",       action=act{ActivatePaneDirection="Left"}},
+  { key = "j", mods = "LEADER",       action=act{ActivatePaneDirection="Down"}},
+  { key = "k", mods = "LEADER",       action=act{ActivatePaneDirection="Up"}},
+  { key = "l", mods = "LEADER",       action=act{ActivatePaneDirection="Right"}},
+  { key = "H", mods = "LEADER|SHIFT", action=act{AdjustPaneSize={"Left", 5}}},
+  { key = "J", mods = "LEADER|SHIFT", action=act{AdjustPaneSize={"Down", 5}}},
+  { key = "K", mods = "LEADER|SHIFT", action=act{AdjustPaneSize={"Up", 5}}},
+  { key = "L", mods = "LEADER|SHIFT", action=act{AdjustPaneSize={"Right", 5}}},
+  { key = "1", mods = "LEADER",       action=act{ActivateTab=0}},
+  { key = "2", mods = "LEADER",       action=act{ActivateTab=1}},
+  { key = "3", mods = "LEADER",       action=act{ActivateTab=2}},
+  { key = "4", mods = "LEADER",       action=act{ActivateTab=3}},
+  { key = "5", mods = "LEADER",       action=act{ActivateTab=4}},
+  { key = "6", mods = "LEADER",       action=act{ActivateTab=5}},
+  { key = "7", mods = "LEADER",       action=act{ActivateTab=6}},
+  { key = "8", mods = "LEADER",       action=act{ActivateTab=7}},
+  { key = "9", mods = "LEADER",       action=act{ActivateTab=8}},
+  { key = "&", mods = "LEADER|SHIFT", action=act{CloseCurrentTab={confirm=true}}},
+  { key = "x", mods = "LEADER",       action=act{CloseCurrentPane={confirm=true}}},
+  { key = "q", mods = "LEADER|CTRL",  action=act.PaneSelect},
+  { key = 's', mods = 'LEADER',       action=act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' }, },
   {
     key = 'S',
     mods = 'LEADER|SHIFT',
@@ -112,9 +126,99 @@ config.keys = {
       end),
     },
   },
+  { key = 'u', mods = 'LEADER|CTRL',
+    action = wezterm.action.CharSelect {
+      copy_on_select = true,
+      copy_to = 'ClipboardAndPrimarySelection',
+    },
+  },
+}
+
+-- copy mode keys
+local copy_mode = nil
+copy_mode = wezterm.gui.default_key_tables().copy_mode
+copy_override = {
+  { key = '?', mods = 'LEADER|SHIFT', action=act.Search{CaseSensitiveString = ""}}
+}
+for _, key in ipairs(copy_override) do
+  table.insert(copy_mode, key)
+end
+
+config.key_tables = {
+    copy_mode = copy_mode,
 }
 
 -- status bar
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
+  end
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return tab_info.active_pane.title
+end
+
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+
+    local colors = {
+      "#343f44",
+      "#3d484d",
+      "#475258",
+      "#4f585e",
+      "#56635f",
+    };
+    local text_fg = "#c0c0c0";
+    local edge_background = colors[1]
+    local background = colors[5]
+    local foreground = text_fg
+
+    if tab.is_active then
+      background = colors[3]
+      foreground = text_fg
+    elseif hover then
+      background = colors[4]
+      foreground = text_fg
+    end
+
+    local edge_foreground = background
+
+    local title = tab_title(tab)
+
+    -- ensure that the titles fit in the available space,
+    -- and that we have room for the edges.
+    title = wezterm.truncate_right(title, max_width - 2)
+
+    return {
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_LEFT_ARROW },
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = title },
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_RIGHT_ARROW },
+    }
+  end
+)
+
 wezterm.on("update-right-status", function(window, pane)
   -- Each element holds the text for a cell in a "powerline" style << fade
   local cells = {};
