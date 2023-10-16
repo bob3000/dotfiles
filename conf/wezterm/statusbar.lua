30e93c1f9dae3caf9901f6a96e1ce299ec27f3ac
Robin Kautz <git@bob3000.net>
2023-10-16 21:30:41 +0200

    powerline tabs

--- conf/wezterm/statusbar.lua
+++ conf/wezterm/statusbar.lua
@@ -7,8 +7,6 @@ M.setup = function(config, wezterm, color_scheme)
 
   -- The filled in variant of the > symbol
   local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-  -- The hollow in variant of the > symbol
-  local LIGHT_RIGHT_ARROW = wezterm.nerdfonts.pl_left_soft_divider
 
   -- This function returns the suggested title for a tab.
   -- It prefers the title that was set via `tab:set_title()`
@@ -25,25 +23,53 @@ M.setup = function(config, wezterm, color_scheme)
     return tab_info.active_pane.title
   end
 
+
   wezterm.on(
     'format-tab-title',
     function(tab, tabs, panes, config, hover, max_width)
       local title = tab_title(tab)
+      -- ensure that the titles fit in the available space,
+      -- and that we have room for the edges.
       title = wezterm.truncate_right(title, max_width - 2)
-      local colors = color_scheme["brights"]
 
-      if tab.is_active then
-        return {
-          { Background = { Color = '#404040' } },
-          { Foreground = { Color = color_scheme["foreground"] } },
-          { Text = ' ' .. title },
-          { Text = LIGHT_RIGHT_ARROW },
-        }
+      -- get last tab index
+      local max_id = 0
+      for _, t in ipairs(tabs) do
+        if t.tab_id > max_id then
+          max_id = t.tab_id
+        end
       end
-      return {
-        { Text = ' ' .. title .. ' '},
-        { Text = LIGHT_RIGHT_ARROW },
-      }
+
+      -- Color palette for the backgrounds of each cell
+      local colors = color_scheme["ansi"]
+      -- Foreground color for the text across the fade
+      local text_fg = color_scheme["foreground"]
+      if tab.is_active then
+        text_fg = color_scheme["background"]
+      end
+
+      -- The elements to be formatted
+      local elements = {};
+
+      -- Translate a cell into elements
+      local function push(text, is_last)
+        local cell_no = math.fmod(tab.tab_id, #colors + 1)
+        table.insert(elements, {Background={Color=colors[cell_no+1]}})
+        table.insert(elements, {Foreground={Color=colors[cell_no]}})
+        table.insert(elements, {Text=SOLID_RIGHT_ARROW})
+        table.insert(elements, {Foreground={Color=text_fg}})
+        table.insert(elements, {Background={Color=colors[cell_no+1]}})
+        table.insert(elements, {Text=" "..text.." "})
+        if is_last then
+          table.insert(elements, {Foreground={Color=colors[cell_no+1]}})
+          table.insert(elements, {Background={Color='#333333'}})
+          table.insert(elements, {Text=SOLID_RIGHT_ARROW})
+        end
+      end
+
+      push(title, tab.tab_id == max_id)
+
+      return elements
     end
   )
 
@@ -85,8 +111,6 @@ M.setup = function(config, wezterm, color_scheme)
     end
     table.insert(cells, window:active_workspace())
 
-    -- The powerline < symbol
-    local LEFT_ARROW = utf8.char(0xe0b3);
     -- The filled in variant of the < symbol
     local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 
@@ -103,11 +127,12 @@ M.setup = function(config, wezterm, color_scheme)
     -- Translate a cell into elements
     local function push(text, is_last)
       local cell_no = num_cells + 1
+      local color_no = math.fmod(cell_no, #colors)
       table.insert(elements, {Foreground={Color=text_fg}})
-      table.insert(elements, {Background={Color=colors[cell_no]}})
+      table.insert(elements, {Background={Color=colors[color_no]}})
       table.insert(elements, {Text=" "..text.." "})
       if not is_last then
-        table.insert(elements, {Foreground={Color=colors[cell_no+1]}})
+        table.insert(elements, {Foreground={Color=colors[color_no+1]}})
         table.insert(elements, {Text=SOLID_LEFT_ARROW})
       end
       num_cells = num_cells + 1
