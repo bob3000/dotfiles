cec85776c2597ec01c002016b766fb10e3593df3
Robin Kautz <git@bob3000.net>
2023-11-10 18:11:21 +0100

    focus first pane in first tab

--- conf/wezterm/startup.lua
+++ conf/wezterm/startup.lua
@@ -1,12 +1,14 @@
 local M = {}
-M.setup = function(config, wezterm)
+M.setup = function(_, wezterm)
   local mux = wezterm.mux
-  local path = os.getenv "PATH"
-  if wezterm.target_triple == "aarch64-apple-darwin" or wezterm.target_triple == "x86_64-apple-darwin" then
-    path = "/opt/homebrew/bin:" .. os.getenv "PATH"
-  end
 
-  wezterm.on("gui-startup", function(cmd)
+  -- in case we want to launch something
+  -- local path = os.getenv "PATH"
+  -- if wezterm.target_triple == "aarch64-apple-darwin" or wezterm.target_triple == "x86_64-apple-darwin" then
+  --   path = "/opt/homebrew/bin:" .. os.getenv "PATH"
+  -- end
+
+  wezterm.on("gui-startup", function(_)
     -- coding workspace
     local coding_dir = wezterm.home_dir .. "/code"
     local general_tab, general_pane, general_window = mux.spawn_window {
@@ -14,35 +16,35 @@ M.setup = function(config, wezterm)
       cwd = coding_dir,
     }
     general_tab:set_title "shell"
-    local term_pane_1 = general_pane:split {
+    general_pane:split {
       direction = "Right",
       size = 0.25,
       cwd = coding_dir,
     }
 
-    local editor_tab, editor_pane, editor_window = general_window:spawn_tab {}
+    local editor_tab, _, _ = general_window:spawn_tab {}
     editor_tab:set_title "editor"
 
-    local cloud_tab, cloud_pane, cloud_window = general_window:spawn_tab {}
+    local cloud_tab, cloud_pane, _ = general_window:spawn_tab {}
     cloud_tab:set_title "cloud"
     cloud_pane:split {
       direction = "Right",
     }
 
-    local remote_tab, remote_pane, remote_window = general_window:spawn_tab {}
+    local remote_tab, remote_pane, _ = general_window:spawn_tab {}
     remote_tab:set_title "remote"
-    local remote_pane_1 = remote_pane:split {
-      size = 0.666,
+    remote_pane:split {
+      size = 0.3,
       direction = "Right",
     }
-    local remote_pane_2 = remote_pane_1:split {
+    remote_pane:split {
       size = 0.5,
       direction = "Right",
     }
 
     -- local workspace
     local local_dir = wezterm.home_dir .. "/code"
-    local local_tab, local_pane, local_window = mux.spawn_window {
+    local local_tab, local_pane, _ = mux.spawn_window {
       workspace = "local",
       cwd = local_dir .. "/dotfiles",
     }
@@ -52,7 +54,12 @@ M.setup = function(config, wezterm)
       cwd = local_dir .. "/dotfiles",
     }
 
+    mux.set_active_workspace "local"
+    local_tab:activate()
+    local_pane:activate()
     mux.set_active_workspace "coding"
+    general_tab:activate()
+    general_pane:activate()
   end)
 end
 
