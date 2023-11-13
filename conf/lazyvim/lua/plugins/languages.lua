b9a145ca20b3308ea3933bfc6934a4dd90d798d0
Robin Kautz <git@bob3000.net>
2023-11-13 15:57:45 +0100

    use native inlay hints

---
+++ conf/lazyvim/lua/plugins/languages.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "simrat39/rust-tools.nvim",
+    opts = {
+      tools = {
+        -- use native inlay hints instead
+        inlay_hints = { auto = false }
+      }
+    }
+  }
+}
