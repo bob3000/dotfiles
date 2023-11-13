24ec5f4228e56aea35299ac7eac9ba9a9086eca5
Robin Kautz <git@bob3000.net>
2023-11-13 16:51:34 +0100

    add auto save plugin

--- conf/lazyvim/lua/plugins/editor.lua
+++ conf/lazyvim/lua/plugins/editor.lua
@@ -63,4 +63,14 @@ return {
       },
     },
   },
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+    keys = { { "<leader>a", "<cmd>ASToggle<cr>", desc = "Toggle auto save" } },
+  },
 }
