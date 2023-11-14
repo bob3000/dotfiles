13bb8b1b9d079315d5cfce8f58c55cce2a453b69
Robin Kautz <git@bob3000.net>
2023-11-14 19:51:44 +0100

    debugging keymaps

--- conf/lazyvim/lua/config/keymaps.lua
+++ conf/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts_no_prefix = {
+  prefix = "",
+  mode = "n", -- NORMAL mode
+  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
+}
+
+local mappings_no_prefix = {
+  ["<M-c>"] = { "<cmd>DapContinue<cr>", "Continue" },
+  ["<M-h>"] = { "<cmd>DapStepOut<cr>", "Step Out" },
+  ["<M-t>"] = { "<cmd>DapTerminate<cr>", "Terminate" },
+  ["<M-j>"] = { "<cmd>DapStepOver<cr>", "Step Over" },
+  ["<M-l>"] = { "<cmd>DapStepInto<cr>", "Step Into" },
+  ["<M-r>"] = { "<cmd>DapToggleRepl<cr>", "Toggle Repl" },
+  ["<M-b>"] = { "<cmd>DapToggleBreakpoint<cr>", "Toggle Breakpoint" },
+  ["<M-u>"] = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle DAP UI" },
+}
+
+require("which-key").register(mappings_no_prefix, opts_no_prefix)
--- conf/lazyvim/lua/plugins/editor.lua
+++ conf/lazyvim/lua/plugins/editor.lua
@@ -67,11 +67,10 @@ return {
     cmd = "ASToggle", -- optional for lazy loading on command
     event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
     opts = {
-      -- your config goes here
-      -- or just leave it empty :)
+      enabled = false,
     },
     keys = {
-      { "<leader>a", "<cmd>ASToggle<cr>", desc = "Toggle auto save" },
+      { "<leader>cS", "<cmd>ASToggle<cr>", desc = "Toggle auto save" },
     },
   },
   {
