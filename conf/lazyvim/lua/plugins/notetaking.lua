af2c17b5560ecd3e0a280b42aedecb8e34c1470f
Robin Kautz <git@bob3000.net>
2023-11-14 09:43:58 +0100

    update journal meta

--- conf/lazyvim/lua/plugins/notetaking.lua
+++ conf/lazyvim/lua/plugins/notetaking.lua
@@ -48,6 +48,7 @@ return {
             "Workspace work",
           },
           j = { "<cmd>lua vim.cmd('Neorg journal today')<cr>", "Journal today" },
+          M = { "<cmd>lua vim.api.nvim_buf_set_text(0, 1, 7, 1, 17, { os.date('%Y-%m-%d') });vim.api.nvim_buf_set_text(0, 8, 9, 8, 33, { os.date('%Y-%m-%dT%H:%M:%S%z') })<cr>", "Update Meta" },
           t = { "<cmd>lua vim.cmd('Neorg journal tomorrow')<cr>", "Journal tomorrow" },
           y = { "<cmd>lua vim.cmd('Neorg journal yesterday')<cr>", "Journal yesterday" },
           i = { "<cmd>lua vim.cmd('Neorg journal toc open')<cr>", "Journal index" },
