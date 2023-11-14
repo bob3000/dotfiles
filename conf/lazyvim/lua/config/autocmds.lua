c74778253367cd86326555f77bd8e87f036b9496
Robin Kautz <git@bob3000.net>
2023-11-14 19:15:49 +0100

    use absolute line numbers in insert mode

--- conf/lazyvim/lua/config/autocmds.lua
+++ conf/lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- switch to absolute line numbers in insert mode
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+  callback = function()
+    vim.opt.relativenumber = false
+    vim.opt.cursorline = false
+  end,
+})
+
+-- switch to relative line numbers in normal mode
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  callback = function()
+    vim.opt.relativenumber = true
+    vim.opt.cursorline = true
+  end,
+})
