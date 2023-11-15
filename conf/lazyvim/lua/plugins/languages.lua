3935a659ff3bdffd8d0506c8ce8ed0507dc8b802
Robin Kautz <git@bob3000.net>
2023-11-15 21:05:58 +0100

    enable inlay hints

--- conf/lazyvim/lua/plugins/languages.lua
+++ conf/lazyvim/lua/plugins/languages.lua
@@ -1,11 +1,17 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = true },
+    },
+  },
+  {
     "simrat39/rust-tools.nvim",
     opts = {
       tools = {
         -- use native inlay hints instead
-        inlay_hints = { auto = false }
-      }
-    }
-  }
+        inlay_hints = { auto = false },
+      },
+    },
+  },
 }
