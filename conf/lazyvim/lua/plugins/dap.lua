7574cf5a7595aa1f280830493d21182a3a29fa98
Robin Kautz <git@bob3000.net>
2023-12-12 09:55:08 +0100

    set up bash debugging and lsp

--- conf/lazyvim/lua/plugins/dap.lua
+++ conf/lazyvim/lua/plugins/dap.lua
@@ -1,6 +1,16 @@
 return {
-  "mfussenegger/nvim-dap",
-  keys = {
-    { "<leader>dB", "<cmd>lua require'dap'.clear_breakpoints()<cr>", desc = "Clear Breakpoints" },
+  {
+    "mfussenegger/nvim-dap",
+    keys = {
+      { "<leader>dB", "<cmd>lua require'dap'.clear_breakpoints()<cr>", desc = "Clear Breakpoints" },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "bash-debug-adapter",
+      },
+    },
   },
 }
