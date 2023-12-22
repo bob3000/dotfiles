0061de64b97f9e6447c608eaa4aed47f12eb006b
Robin Kautz <git@bob3000.net>
2023-12-22 17:01:59 +0100

    feat(neovim): light colorscheme

--- conf/lazyvim/lua/plugins/colorscheme.lua
+++ conf/lazyvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
   { "neanias/everforest-nvim" },
+  { "yorik1984/newpaper.nvim", priority = 1000, config = true },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "newpaper",
     },
   },
 }
