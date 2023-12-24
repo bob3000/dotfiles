dc63d8b6b3d3a7de2ea35eaecd4b2eb9d1035d03
Robin Kautz <git@bob3000.net>
2023-12-24 12:08:24 +0100

    feat(wezterm): use fira code

--- conf/wezterm/fonts.lua
+++ conf/wezterm/fonts.lua
@@ -1,13 +1,13 @@
 local M = {}
 M.setup = function(config, wezterm)
   config.font = wezterm.font_with_fallback {
-    {
-      family = "JetBrains Mono",
-      weight = "Light",
-      harfbuzz_features = { "calt=1", "ss01" },
-    },
+    -- {
+    --   family = "JetBrains Mono",
+    --   weight = "Light",
+    --   harfbuzz_features = { "calt=1", "ss01" },
+    -- },
     -- { family = 'Terminus', weight = 'Bold' },
-    -- { family = "Fira Code Nerd Font Mono", weight = "Light", harfbuzz_features = { "calt=1", "ss01" } },
+    { family = "Fira Code Nerd Font Mono", weight = "Light", harfbuzz_features = { "calt=1", "ss01" } },
     { family = "DejaVu Sans Mono", weight = "Medium" },
     "Droid Sans Fallback",
     "Noto Color Emoji",
@@ -20,7 +20,7 @@ M.setup = function(config, wezterm)
   config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
   config.font_size = 15.0
   config.line_height = 0.9
-  config.cell_width = 0.95
+  -- config.cell_width = 0.95
   config.underline_thickness = 2.0
   -- config.underline_position = -1.1
 end
