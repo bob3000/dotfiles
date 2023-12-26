93053d94ef7f4048a728c25f4e876a6830e7fa79
Robin Kautz <git@bob3000.net>
2023-12-26 19:10:18 +0100

    feat(neovim): system dependent font config

--- conf/wezterm/fonts.lua
+++ conf/wezterm/fonts.lua
@@ -1,28 +1,43 @@
 local M = {}
+local fonts = {
+  -- {
+  --   family = "JetBrains Mono",
+  --   weight = "Light",
+  --   harfbuzz_features = { "calt=1", "ss01" },
+  -- },
+  -- { family = 'Terminus', weight = 'Bold' },
+  -- { family = "Cascadia Code", weight = "Medium", harfbuzz_features = { "calt=1", "ss01" } },
+  -- { family = "VictorMono Nerd Font Mono", weight = "Medium", harfbuzz_features = { "calt=1", "ss01" } },
+  "Droid Sans Fallback",
+  "Noto Color Emoji",
+  -- "Noto Sans Javanese",
+  -- "Noto Serif Tibetan",
+  -- "Noto Sans Ol Chiki",
+}
+
+local macos_fonts = {
+  { family = "FiraCode Nerd Font Mono", weight = "Medium", harfbuzz_features = { "calt=1", "ss01" } },
+}
+
+local linux_fonts = {
+  { family = "Fira Code Nerd Font Mono", weight = "Medium", harfbuzz_features = { "calt=1", "ss01" } },
+}
+
 M.setup = function(config, wezterm)
-  config.font = wezterm.font_with_fallback {
-    -- {
-    --   family = "JetBrains Mono",
-    --   weight = "Light",
-    --   harfbuzz_features = { "calt=1", "ss01" },
-    -- },
-    -- { family = 'Terminus', weight = 'Bold' },
-    { family = "Fira Code Nerd Font Mono", weight = "Light", harfbuzz_features = { "calt=1", "ss01" } },
-    { family = "DejaVu Sans Mono", weight = "Medium" },
-    "Droid Sans Fallback",
-    "Noto Color Emoji",
-    "Noto Sans Javanese",
-    "Noto Serif Tibetan",
-    "Noto Sans Ol Chiki",
-  }
+  if wezterm.target_triple == "aarch64-apple-darwin" or wezterm.target_triple == "x86_64-apple-darwin" then
+    fonts = table.pack(table.unpack(macos_fonts), table.unpack(fonts))
+  elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+    fonts = table.pack(table.unpack(linux_fonts), table.unpack(fonts))
+  end
+  config.font = wezterm.font_with_fallback(fonts)
   config.freetype_load_flags = "NO_HINTING"
   config.freetype_load_target = "Light"
   config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
   config.font_size = 15.0
   config.line_height = 0.9
-  -- config.cell_width = 0.95
+  config.cell_width = 0.95
   config.underline_thickness = 2.0
-  -- config.underline_position = -1.1
+  config.underline_position = -1.1
 end
 
 return M
