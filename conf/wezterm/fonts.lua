064bc2509d72451bb907467ece5981053c972764
Robin Kautz <git@bob3000.net>
2023-12-30 11:32:19 +0100

    fix(wezterm): adjust font size

--- conf/wezterm/fonts.lua
+++ conf/wezterm/fonts.lua
@@ -33,7 +33,7 @@ M.setup = function(config, wezterm)
   config.freetype_load_flags = "NO_HINTING"
   config.freetype_load_target = "Light"
   config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
-  config.font_size = 15.0
+  config.font_size = 14.0
   config.line_height = 0.9
   config.cell_width = 0.95
   config.underline_thickness = 2.0
