b87f1fc1bcbca5d4ccbb81fb7a631d8498802b85
Robin Kautz <git@bob3000.net>
2024-01-21 16:51:11 +0100

    feat(wezterm): check if running on nixos

--- conf/wezterm/options.lua
+++ conf/wezterm/options.lua
@@ -12,15 +12,27 @@ M.setup = function(wezterm, config)
   --   },
   -- }
 
+  function file_exists(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+  end
+
   if wezterm.target_triple == "aarch64-apple-darwin" or wezterm.target_triple == "x86_64-apple-darwin" then
     config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
     config.set_environment_variables = {
       SHELL = "/opt/homebrew/bin/fish",
     }
   elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
-    config.default_prog = { "/usr/bin/fish", "-l" }
+
+    local fish = nil
+    if file_exists("/nix") then
+      fish = "/run/current-system/sw/bin/fish"
+    else
+      fish = "/usr/bin/fish"
+    end
+    config.default_prog = { fish, "-l" }
     config.set_environment_variables = {
-      SHELL = "/usr/bin/fish",
+      SHELL = fish,
     }
   end
 end
