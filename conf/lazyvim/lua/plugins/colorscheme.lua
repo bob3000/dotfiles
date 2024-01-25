efbc884297e0fc736f9fd75eb8096d1585f85066
Robin Kautz <git@bob3000.net>
2024-01-25 16:18:19 +0100

    feat(neovim): enable transprency

--- conf/lazyvim/lua/plugins/colorscheme.lua
+++ conf/lazyvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,12 @@
 return {
-  { "neanias/everforest-nvim" },
+  {
+    "neanias/everforest-nvim",
+    config = function()
+      require("everforest").setup({
+        transparent_background_level = 2,
+      })
+    end,
+  },
   { "yorik1984/newpaper.nvim", event = "VeryLazy" },
   {
     "LazyVim/LazyVim",
