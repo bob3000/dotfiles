18921a7b9ca60a0e85dd8ca41b441e4bd4ea54f3
Robin Kautz <git@bob3000.net>
2024-01-27 19:50:54 +0100

    feat(nixos): enable bluetooth and pipewire

--- conf/nixos/configuration.nix
+++ conf/nixos/configuration.nix
@@ -66,9 +66,22 @@
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
+  hardware = {
+    pulseaudio.enable = false;
+    bluetooth.enable = true;
+    sane.enable = true;
+  };
+
   # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
@@ -127,6 +140,7 @@
     brave
     cachix
     chars
+    chromium
     clinfo
     cmake
     codespell
