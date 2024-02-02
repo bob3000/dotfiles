a09d5aef6450e07b770c63aa907d0613c5b387b2
Robin Kautz <git@bob3000.net>
2024-02-02 10:31:05 +0100

    fix(neovim): disable some ui plugins

--- lazyvim/.config/nvim/lua/plugins/languages.lua
+++ lazyvim/.config/nvim/lua/plugins/languages.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       ensure_installed = {
         "fish",
+        "groovy",
         "make",
         "nix",
         "ruby",
---
+++ lazyvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    enabled = false,
+  },
+  {
+    "stevearc/dressing.nvim",
+    enabled = false,
+  },
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
+    "RRethy/vim-illuminate",
+    enabled = false,
+  },
+}
