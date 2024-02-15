bcc9a3e58b8606265f7ad39009c5396e99fa0c6f
Robin Kautz <git@bob3000.net>
2024-02-15 19:27:42 +0100

    fix(fish): prefer nvim installed with bob

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -99,6 +99,7 @@ if test -d /home/linuxbrew/.linuxbrew
     set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
     set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
     set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH
+    set -gx PATH "$HOME/.local/share/bob/nvim-bin:$PATH"
     set -q MANPATH; or set MANPATH ''
     set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH
     set -q INFOPATH; or set INFOPATH ''
