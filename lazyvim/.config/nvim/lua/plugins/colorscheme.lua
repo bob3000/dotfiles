ed04549b21b40c707ebd7bb10386090b33390942
Robin Kautz <git@bob3000.net>
2024-02-22 10:20:31 +0100

    feat(neovim): colorscheme tweaks

--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,12 @@ return {
     "neanias/everforest-nvim",
     config = function()
       require("everforest").setup({
+        background = "hard",
+        italics = true,
+        sign_column_background = "grey",
+        ui_contrast = "high",
+        dim_inactive_windows = true,
+        show_eob = false,
         transparent_background_level = 2,
       })
     end,
