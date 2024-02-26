df82a88e8fb3f3ef78cb9c8bf0ebfc9110f6547e
Robin Kautz <git@bob3000.net>
2024-02-26 18:10:50 +0100

    feat(neovim): local debugger config (launch.json)

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -4,6 +4,9 @@ return {
     keys = {
       { "<leader>dB", "<cmd>lua require'dap'.clear_breakpoints()<cr>", desc = "Clear Breakpoints" },
     },
+    opts = function ()
+      require("dap.ext.vscode").load_launchjs(nil, { codelldb = {'c', 'cpp'} })
+    end
   },
   {
     "williamboman/mason.nvim",
--- nvim/.config/nvim/lua/plugins/languages.lua
+++ nvim/.config/nvim/lua/plugins/languages.lua
@@ -33,57 +33,90 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^3", -- Recommended
     ft = { "rust" },
-    opts = {
-      server = {
-        on_attach = function(client, bufnr)
-          -- register which-key mappings
-          local wk = require("which-key")
-          wk.register({
-            ["<leader>cR"] = {
-              function()
-                vim.cmd.RustLsp("codeAction")
-              end,
-              "Code Action",
-            },
-            ["<leader>dr"] = {
-              function()
-                vim.cmd.RustLsp("debuggables")
-              end,
-              "Rust debuggables",
-            },
-          }, { mode = "n", buffer = bufnr })
-        end,
-        settings = {
-          -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              runBuildScripts = true,
-            },
-            -- Add clippy lints for Rust.
-            checkOnSave = {
-              allFeatures = true,
-              command = "clippy",
-              extraArgs = { "--no-deps" },
-            },
-            procMacro = {
-              enable = true,
-              ignored = {
-                ["async-trait"] = { "async_trait" },
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
+    opts = function()
+      local ok, mason_registry = pcall(require, "mason-registry")
+      local adapter ---@type any
+      if ok then
+        -- rust tools configuration for debugging support
+        local codelldb = mason_registry.get_package("codelldb")
+        local extension_path = codelldb:get_install_path() .. "/extension/"
+        local codelldb_path = extension_path .. "adapter/codelldb"
+        local liblldb_path = ""
+        if vim.loop.os_uname().sysname:find("Windows") then
+          liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+        elseif vim.fn.has("mac") == 1 then
+          liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+        else
+          liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+        end
+        adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb_path, liblldb_path)
+        require("dap.ext.vscode").load_launchjs(nil, { codelldb = {'rust'} })
+      end
+      return {
+        dap = {
+          adapter = adapter,
+        },
+        tools = {
+          on_initialized = function()
+            vim.cmd([[
+                  augroup RustLSP
+                    autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+                    autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+                    autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+                  augroup END
+                ]])
+          end,
+        },
+        server = {
+          on_attach = function(client, bufnr)
+            -- register which-key mappings
+            local wk = require("which-key")
+            wk.register({
+              ["<leader>cR"] = {
+                function()
+                  vim.cmd.RustLsp("codeAction")
+                end,
+                "Code Action",
+              },
+              ["<leader>dr"] = {
+                function()
+                  vim.cmd.RustLsp("debuggables")
+                end,
+                "Rust debuggables",
+              },
+            }, { mode = "n", buffer = bufnr })
+          end,
+          settings = {
+            -- rust-analyzer language server configuration
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
+              -- Add clippy lints for Rust.
+              checkOnSave = {
+                allFeatures = true,
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
               },
             },
           },
         },
-      },
-    },
+      }
+    end,
     config = function(_, opts)
       vim.g.rustaceanvim = vim.tbl_deep_extend("force", {}, opts or {})
     end,
   },
-
   -- Correctly setup lspconfig for Rust 🚀
   {
     "neovim/nvim-lspconfig",
@@ -96,5 +129,3 @@ return {
     },
   },
 }
-
-
