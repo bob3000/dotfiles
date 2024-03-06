b153b90ef4ff0037bb0c1cf9769ac9ec8d5d2247
Robin Kautz <git@bob3000.net>
2024-03-06 11:34:16 +0100

    feat(neovim): install required linters and formatters

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,14 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "autotools-language-server",
         "bash-language-server",
+        "black",
+        "gofumpt",
+        "goimports",
+        "prettier",
+        "shfmt",
+        "stylua",
       },
     },
   },
