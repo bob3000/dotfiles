692a2b4ba4f20b039e13896dfefe2f63f7f57bf8
Robin Kautz <git@bob3000.net>
2024-03-08 17:57:15 +0100

    feat(neovim): MasonInstallAll

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,20 +1,43 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "autotools-language-server",
-        "bash-debug-adapter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
         "bash-language-server",
         "black",
+        "clangd",
         "codelldb",
         "debugpy",
+        "delve",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
         "gofumpt",
         "goimports",
+        "gopls",
+        "hadolint",
+        "js-debug-adapter",
+        "json-lsp",
+        "lua-language-server",
+        "markdownlint",
+        "marksman",
         "prettier",
+        "pyright",
+        "ruff-lsp",
+        "rust-analyzer",
         "shfmt",
         "stylua",
-      },
-    },
+        "taplo",
+        "terraform-ls",
+        "typescript-language-server",
+        "yaml-language-server",
+        "zls",
+      })
+
+      vim.api.nvim_create_user_command("MasonInstallAll", function()
+        if opts.ensure_installed and #opts.ensure_installed > 0 then
+          vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+        end
+      end, {})
+    end,
   },
 }
