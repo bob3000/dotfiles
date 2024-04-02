7e720bff5d139192bf3f1d7ed2ff3e9e2ef5188b
Robin Kautz <git@bob3000.net>
2024-04-02 18:27:20 +0200

    feat(neovim): neorg completion

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -5,6 +5,7 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
+      table.insert(opts.sources, { name = "neorg" })
     end,
   },
 }
