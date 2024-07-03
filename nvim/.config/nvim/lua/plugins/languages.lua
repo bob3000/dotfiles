e631d7fa49139a1ec938ec08f7bdc521abdd17f0
Robin Kautz <git@bob3000.net>
2024-07-03 09:07:18 +0200

    feat(neovim): plugin upgrade

--- nvim/.config/nvim/lua/plugins/languages.lua
+++ nvim/.config/nvim/lua/plugins/languages.lua
@@ -1,5 +1,17 @@
 return {
   {
+    "mfussenegger/nvim-dap-python",
+    config = function()
+      if vim.fn.has("win32") == 1 then
+        require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/Scripts/pythonw.exe"))
+      else
+        require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/bin/python"), {
+          pythonPath = vim.fn.getcwd() .. "/.venv/bin/python",
+        })
+      end
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
