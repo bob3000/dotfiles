c33f6244ce36b00bbd08da52e2d8d468f75f6751
Robin Kautz <git@bob3000.net>
2024-08-13 09:39:48 +0200

    feat(neovim): plugin upgrade

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   {
     "neanias/everforest-nvim",
+    lazy = true,
     config = function()
       require("everforest").setup({
         background = "hard",
@@ -10,11 +11,13 @@ return {
         dim_inactive_windows = true,
         show_eob = false,
         transparent_background_level = 2,
+        dim_inactive_windows = true,
       })
     end,
   },
   {
     "ellisonleao/gruvbox.nvim",
+    lazy = true,
     config = function()
       require("gruvbox").setup({
         contrast = "soft", -- can be "hard", "soft" or empty string
--- nvim/.config/nvim/lua/plugins/languages.lua
+++ nvim/.config/nvim/lua/plugins/languages.lua
@@ -57,7 +57,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      inlay_hints = { enabled = true },
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
     },
   },
 }
