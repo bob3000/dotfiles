36ef50c8fb2a58e68605c03e55d01063e8f50aae
Robin Kautz <git@bob3000.net>
2024-09-13 16:06:16 +0200

    fixup! fix(neovim): comment currently unused languages

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -40,6 +40,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.test.core" },
+    -- { import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.util.dot" },
