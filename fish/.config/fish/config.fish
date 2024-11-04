a14b5389e58e69810da797be93c3b86312d0e0ed
Robin Kautz <git@bob3000.net>
2024-11-04 15:55:44 +0100

    fix(fzf): bg colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -66,7 +66,7 @@ set --export PATH "$PYENV_ROOT/bin:$PATH"
 set --export PATH "/opt/homebrew/opt/libpq/bin:$PATH"
 command -q luarocks && eval "$(luarocks path --bin)"
 
-set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --bind ctrl-k:preview-up,ctrl-j:preview-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down'
+set --export FZF_DEFAULT_OPTS '--color "hl:-1:underline,hl+:-1:underline:reverse,bg+:-1:underline:reverse" --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --bind ctrl-k:preview-up,ctrl-j:preview-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down'
 set --export FZF_DEFAULT_COMMAND 'fd --hidden --type f'
 set --export FZF_CTRL_T_COMMAND 'fd --color always --exclude "Library/*" . $HOME'
 set --export FZF_CTRL_T_OPTS '--ansi --multi --prompt="File> " --preview "preview_files {}"'
