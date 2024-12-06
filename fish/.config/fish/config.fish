75b9b96b35f9f637436defc204bc95b0e08a1978
Robin Kautz <git@bob3000.net>
2024-12-06 11:43:37 +0100

    fix(fish): fix keybindings ec2 fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,11 +36,12 @@ end
 
 function aws_ec2_select --description "Get EC2 instance information"
     set -f col $(aws-ec2-describe | fzf --prompt 'Instance> ' --ansi \
-    --bind 'alt-i:become(echo {1})' \
-    --bind 'alt-p:become(echo {2})' \
-    --bind 'alt-P:become(echo {3})' \
+    --header 'InstanceId <enter> / IP <alt-i> / Public IP <alt-p> / Name <alt-n>' \
+    --bind 'enter:become(echo {1})' \
+    --bind 'alt-i:become(echo {2})' \
+    --bind 'alt-p:become(echo {3})' \
     --bind 'alt-n:become(echo {4})')
-  printf "%s" $col
+    printf "%s" $col
 end
 
 function aws_ec2_select_replace --description "replace the current command token"
