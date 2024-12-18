98977dbc3a00c3effbabdae11b7ada696bd56c9a
Robin Kautz <git@bob3000.net>
2024-12-18 09:37:13 +0100

    fix(neovim): reconfigure noice

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -76,6 +76,19 @@ return {
     enabled = false,
   },
   {
+    "folke/noice.nvim",
+    opts = {
+      messages = {
+        enabled = true, -- enables the Noice messages UI
+        view = "mini", -- default view for messages
+        view_error = "mini", -- view for errors
+        view_warn = "mini", -- view for warnings
+        view_history = "messages", -- view for :messages
+        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+      },
+    },
+  },
+  {
     "s1n7ax/nvim-window-picker",
     name = "window-picker",
     event = "VeryLazy",
