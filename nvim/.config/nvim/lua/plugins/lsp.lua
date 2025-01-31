43d5a3a7a5d0b7257c79b255b9af8f6f43c7813d
Robin Kautz <git@bob3000.net>
2025-01-31 09:38:23 +0100

    feat(neovim): disable some annoying ls

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,18 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {
+          mason = false,
+        },
+        yamlls = {
+          mason = false,
+        },
+      },
+    },
+  },
+  {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
