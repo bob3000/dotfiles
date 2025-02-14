a8e8932b45c6bc786b28c889c97eef0e551cfb4a
Robin Kautz <git@bob3000.net>
2025-02-14 18:34:07 +0100

    fix(neovim): tweak color scheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,20 @@
 return {
   {
     "neanias/everforest-nvim",
-    lazy = true,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("everforest").setup({
-        background = "hard",
+        background = "soft",
         italics = true,
         sign_column_background = "grey",
         ui_contrast = "high",
-        dim_inactive_windows = true,
+        dim_inactive_windows = false,
         show_eob = false,
         transparent_background_level = 2,
+        colours_override = function(palette)
+          palette.bg4 = "#a6b0a0"
+        end,
       })
     end,
   },
