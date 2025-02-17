e28a63d7fc711b15b8c74fb4c55e1fd12b73172d
Robin Kautz <git@bob3000.net>
2025-02-17 16:35:13 +0100

    fix(kitty): use everforest light theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("everforest").setup({
-        background = "soft",
+        background = "hard",
         italics = true,
         sign_column_background = "grey",
         ui_contrast = "high",
