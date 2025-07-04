538f21cdd18282b691a56e145f4177a1d352efb5
Robin Kautz <git@bob3000.net>
2025-07-04 12:42:10 +0200

    feat(kvim): replace fidget with aerial

--- kvim/.config/nvim/lua/plugins/lualine.lua
+++ kvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,15 @@
 return {
   {
     'nvim-lualine/lualine.nvim',
-    opts = {},
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      sections = {
+        lualine_c = { 'aerial' },
+        lualine_z = {
+          "vim.tbl_contains({'v', 'V'}, vim.fn.mode()) and string.format('%d words', vim.fn.wordcount()['visual_words'])",
+          "lsp_status",
+        },
+      },
+    },
   },
 }
