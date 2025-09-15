8f0ab56a0bd40835635a86fdab6ac14991027d97
Robin Kautz <git@bob3000.net>
2025-09-15 21:32:26 +0200

    feat(nvim): plugin upgrade

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -353,7 +353,10 @@ return {
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500
+        },
         list = {
           selection = {
             preselect = true,
@@ -362,7 +365,8 @@ return {
         },
         menu = {
           enabled = true,
-          auto_show = false,
+          auto_show = true,
+          auto_show_delay_ms = 500,
         },
         ghost_text = {
           show_with_menu = true,
