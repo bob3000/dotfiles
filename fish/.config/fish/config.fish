6af3a31d0398d7fa02e5998e623b3666c06d4eae
Robin Kautz <git@bob3000.net>
2026-03-17 15:36:33 +0100

    fix(fish/k9s/lazygit): revert to transparent theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -87,14 +87,9 @@ set --export PATH "$HOMEBREW_PREFIX/opt/postgresql@*/bin:$PATH"
 set --export PATH "$HOMEBREW_PREFIX/opt/llvm/bin:$PATH"
 command -q luarocks && eval "$(luarocks path --bin)"
 
-set --export FZF_DEFAULT_OPTS '--color "bg+:-1:underline,fg+:-1:underline,hl+:-1:underline"'\
+set --export FZF_DEFAULT_OPTS '--color "bg+:-1:underline,fg+:-1:underline,hl+:-1:underline" --ansi'\
 ' --cycle --layout=reverse --border --height=90% --preview-window=wrap --pointer="" --marker=" "'\
-' --bind ctrl-k:preview-up,ctrl-j:preview-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down'\
-' --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8'\
-' --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC'\
-' --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8'\
-' --color=selected-bg:#45475A'\
-' --color=border:#6C7086,label:#CDD6F4'
+' --bind ctrl-k:preview-up,ctrl-j:preview-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down'
 set --export FZF_DEFAULT_COMMAND 'fd --hidden --type f'
 set --export FZF_CTRL_T_COMMAND 'fd --color always --exclude "Library/*" . $HOME'
 set --export FZF_CTRL_T_OPTS '--ansi --multi --prompt="File> " --preview "fzf-preview.sh {}"'
