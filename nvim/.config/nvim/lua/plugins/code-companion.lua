67e97e836c87cbee72689887f41d6a225a6f3741
Robin Kautz <git@bob3000.net>
2026-05-06 18:19:15 +0200

    feat(nvim): enable companion

--- nvim/.config/nvim/lua/plugins/code-companion.lua
+++ nvim/.config/nvim/lua/plugins/code-companion.lua
@@ -16,7 +16,7 @@ vim.cmd([[cab cc CodeCompanion]])
 return {
   {
     'olimorris/codecompanion.nvim',
-    enabled = false,
+    enabled = true,
     lazy = false,
     version = '*',
     depedendencies = {
@@ -44,38 +44,6 @@ return {
       },
     },
     opts = {
-      interactions = {
-        chat = {
-          opts = {
-            ---@param ctx CodeCompanion.SystemPrompt.Context
-            ---@return string
-            system_prompt = function(ctx)
-              return ctx.default_system_prompt
-                .. [[Additional context: Mildly adjust your way of
-                communicating to the computer on the spaceship enterprise in
-                the TV series Star Trek Next Generation. Your writing style
-                shall be dry and computer like. Use technical language. When
-                becoming aware that you committed an error or suffered from a
-                hallucination start your response with "correction" or "compute
-                error".
-                
-                Be brief and to the point unless I use phrases like `I'm
-                learning` or `this is an exercise` which is a signal to you to
-                respond more verbose explain in detail.
-                
-                Show references to relevant documentation pages when
-                recommending specific configurations or settings. Make sure
-                you're recommending up to data settings and practices. Don't
-                recommend deprecated settings. Point out if I'm using
-                deprecated or obsolete setting or diverging from common best
-                practices. If you need to verify information or if you're
-                lacking inforamtion you can use @{web_search} to obtain more
-                information.
-]]
-            end,
-          },
-        },
-      },
       strategies = {
         chat = {
           adapter = 'ollama',
