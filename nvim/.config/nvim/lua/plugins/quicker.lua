b5a03a6487b99d1e035cd4ba475760b73c1ffd5c
Robin Kautz <git@bob3000.net>
2026-05-06 09:21:41 +0200

    feat(nvim): auto update quickfix and loc list

--- nvim/.config/nvim/lua/plugins/quicker.lua
+++ nvim/.config/nvim/lua/plugins/quicker.lua
@@ -28,7 +28,14 @@ return {
       {
         '<leader>xl',
         function()
-          require('quicker').toggle({ loclist = true })
+          local quicker = require('quicker')
+          local win = vim.api.nvim_get_current_win()
+          if quicker.is_open(win) then
+            quicker.close({ loclist = true })
+          else
+            vim.diagnostic.setloclist({ open = false })
+            quicker.open({ loclist = true, focus = false })
+          end
         end,
         desc = 'Toggle loclist list',
       },
@@ -36,15 +43,30 @@ return {
         '<leader>xd',
         function()
           local quicker = require('quicker')
-
           if quicker.is_open() then
             quicker.close()
           else
-            vim.diagnostic.setqflist()
+            vim.diagnostic.setqflist({ open = false })
+            quicker.open({ focus = false })
           end
         end,
         desc = 'Toggle diagnostics',
       },
     },
+    config = function()
+      vim.api.nvim_create_autocmd('DiagnosticChanged', {
+        callback = function()
+          local quicker = require('quicker')
+          local win = vim.api.nvim_get_current_win()
+          if quicker.is_open(win) then
+            vim.diagnostic.setloclist()
+            quicker.refresh(win)
+          elseif quicker.is_open() then
+            vim.diagnostic.setqflist()
+            quicker.refresh()
+          end
+        end,
+      })
+    end,
   },
 }
