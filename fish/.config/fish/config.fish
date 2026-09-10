add08cb19995269e9202120e88015210acab234a
Robin Kautz <git@bob3000.net>
2026-09-10 09:09:44 +0200

    feat(starship): keep duration in right prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -142,6 +142,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     function starship_transient_rprompt_func
         starship module time
+        starship module $argv cmd_duration
     end
     function starship_transient_prompt_func
         starship module character
